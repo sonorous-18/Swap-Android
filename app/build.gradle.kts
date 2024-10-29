b09063831febdd4f8341d8c1d6dd02f03ede6da7
seo1016 <seoseunghun3@gmail.com>
2024-10-30 04:04:31 +0900

    chore :: Add viewmodel and navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,8 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     implementation(libs.androidx.material.icons.extended)
+
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    implementation(libs.androidx.navigation.compose)
 }
