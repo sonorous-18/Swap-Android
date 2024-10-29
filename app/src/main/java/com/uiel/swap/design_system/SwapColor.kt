f66201cdc3c72c5cbf58ceb84cd5e97fa25f5652
uiel <parkuiery@gmail.com>
2024-10-29 19:35:46 +0900

    feat :: SwapColor

---
+++ app/src/main/java/com/uiel/swap/design_system/SwapColor.kt
@@ -0,0 +1,18 @@
+package com.uiel.swap.design_system
+
+import androidx.compose.ui.graphics.Color
+
+object SwapColor {
+    val gray0 = Color(0xFFffffff)
+    val gray100 = Color(0xFFF5F6F8)
+    val gray200 = Color(0xFFEFF0F2)
+    val gray300 = Color(0xFFB4B5B7)
+    val gray450 = Color(0xFF8F9094)
+    val gray600 = Color(0xFF666669)
+    val gray800 = Color(0xFF3C3C3E)
+    val gray900 = Color(0xFF1C1C1D)
+    val main300 = Color(0xFFB096ED)
+    val main500 = Color(0xFF683BD1)
+    val main700 = Color(0xFF3E1D8B)
+    val error = Color(0xFFDF454A)
+}
