acbc5d1a34c870e7ef01896773490c500114624a
seo1016 <seoseunghun3@gmail.com>
2024-10-30 19:13:19 +0900

    commit

--- app/src/main/java/com/uiel/swap/MainActivity.kt
+++ app/src/main/java/com/uiel/swap/MainActivity.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
 import com.uiel.swap.ui.main.MainScreen
 import com.uiel.swap.ui.theme.SwapTheme
+import android.os.Handler
+import android.widget.Toast
 
 class MainActivity : ComponentActivity() {
+//    private var doubleBackToExitPressedOnce = false
+//    private val handler = Handler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,4 +23,16 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+//    override fun onBackPressed() {
+//        if (doubleBackToExitPressedOnce) {
+//            super.onBackPressed()
+//            return
+//        }
+//
+//        this.doubleBackToExitPressedOnce = true
+//        Toast.makeText(this, "한 번 더 뒤로가기하면 종료됩니다.", Toast.LENGTH_SHORT).show()
+//
+//        handler.postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
+//    }
 }
