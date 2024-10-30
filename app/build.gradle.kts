8c4ac40416291347615d0d4c1054e7277c52d9d1
seo1016 <seoseunghun3@gmail.com>
2024-10-31 01:17:42 +0900

    chore :: add coil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,4 +84,6 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation("io.coil-kt:coil-compose:2.5.0")
 }
